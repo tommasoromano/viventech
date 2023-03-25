512fa973f419a05d682cb1e0a4a224ecc27f1894
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2023-03-25 10:26:45 +0100

    first

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+
+export function Footer() {
+  return(
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <div>
+      <p>Copyright © 2023 - All right reserved by viventech</p>
+    </div>
+  </footer>
+  )
+}
