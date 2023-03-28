87436f4c9ff9936c87e68039a9789dfede944662
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2023-03-28 17:10:09 +0200

    alerts

--- src/components/deviceCard.tsx
+++ src/components/deviceCard.tsx
@@ -8,27 +8,56 @@ export interface DeviceCardProps {
   icon: JSX.Element;
   toggle: boolean;
   watt: number;
+  seconds: number;
+  planned: number;
+  alerts: number;
 }
 
-export function DeviceCard({title, icon, toggle, watt}: DeviceCardProps) {
+function formatDuration(durationInSeconds:number) {
+  const hours = Math.floor(durationInSeconds / 3600);
+  const minutes = Math.floor((durationInSeconds % 3600) / 60);
+  const seconds = durationInSeconds % 60;
+  
+  const hoursStr = hours > 0 ? hours + "h " : "";
+  const minutesStr = minutes > 0 ? minutes + "m " : "1m";
+  const secondsStr = seconds + "s";
+
+  return hoursStr + minutesStr;
+}
+
+export function DeviceCard({title, icon, toggle, watt, seconds, planned, alerts}: DeviceCardProps) {
 
   const [isToggle, setIsToggle] = useState(toggle);
 
   return (
     <div className={"card shadow-xl "+(isToggle ? "bg-success" : "")}>
       <div className={"card-body flex flex-row justify-between"}>
-        <div className="w-10 rounded-full text-center text-primary text-xl">
+        {/* <div className="w-10 rounded-full text-center text-primary text-xl">
         {icon}
-        </div>
-        {/* <div className="card-title">{title}</div> */}
-        <p className="text-left"> <span className="text-md">{title}</span><br/>{isToggle ? watt.toString() : "0"}w</p>
+        </div> */}
+        <p className="text-left">  
+        <span className="text-xl text-primary mr-2">{icon}</span>
+        <span className="text-md font-bold mr-2">{title}</span>
+        <span className="italic opacity-75 text-sm">
+          {isToggle ? watt.toString() : "0"}w
+        </span>
+        <br/>
+        <span className="opacity-75 text-sm">
+          {planned.toString()} plans - {alerts.toString()} alerts
+        </span>
+        </p>
         {/* <div className="card-subtitle text-neutral-content">Card Subtitle</div> */}
-        <Toggle
-          label=""
-          defaultChecked={toggle}
-          onChange={() => setIsToggle(!isToggle)}
-          checked={isToggle}
-        />
+        <div className="flex flex-col justify-between">
+          <Toggle
+            label=""
+            defaultChecked={toggle}
+            onChange={() => setIsToggle(!isToggle)}
+            checked={isToggle}
+          />
+          <p className="text-right italic opacity-75 text-sm">  
+          {isToggle ? formatDuration(seconds) : ""}
+          </p>
+        </div>
         {/* <KeyboardArrowRight/> */}
       </div>
     </div>
--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -60,53 +60,95 @@ export const data = {
 
 export default function Demo() {
 
-  const phoneContent = (
+  const phoneDevices = (
     <div className="container mx-auto min-h-full p-4 mt-12">
       <div className="grid grid-cols-1 gap-4">
         <h1 className="text-xl text-center font-bold">Your devices:</h1>
-        <DeviceCard title={"Fridge"} 
-        icon={<Kitchen/>} 
-        toggle={true} watt={155}/>
-        <DeviceCard title={"TV 1"} 
-        icon={<Tv/>} 
-        toggle={true} watt={71}/>
-        <DeviceCard title={"Dishwasher"} 
-        icon={<LocalLaundryService/>} 
-        toggle={false} watt={76}/>
-        <DeviceCard title={"Kitchen Light"} 
-        icon={<Lightbulb/>} 
-        toggle={false} watt={1}/>
-        <DeviceCard title={"Bedroom 1 Light"} 
-        icon={<Lightbulb/>} 
-        toggle={false} watt={2}/>
+        <DeviceCard
+          title={"Fridge"}
+          icon={<Kitchen />}
+          toggle={true}
+          watt={155}
+          seconds={4560}
+          planned={0} alerts={1}          />
+        <DeviceCard
+          title={"TV 1"}
+          icon={<Tv />}
+          toggle={true}
+          watt={71}
+          seconds={540}
+          planned={2} alerts={2}          />
+        <DeviceCard
+          title={"Dishwasher"}
+          icon={<LocalLaundryService />}
+          toggle={false}
+          watt={76}
+          seconds={340}
+          planned={3} alerts={1}          />
+        <DeviceCard
+          title={"Kitchen Light"}
+          icon={<Lightbulb />}
+          toggle={false}
+          watt={1}
+          seconds={870}
+          planned={0} alerts={1}          />
+        <DeviceCard
+          title={"Bedroom 1 Light"}
+          icon={<Lightbulb />}
+          toggle={false}
+          watt={2}
+          seconds={980}
+          planned={0} alerts={1}          />
       </div>
     </div>
   );
-  // const phoneContent = (
-  //   <Line options={options} data={data} />
-  // );
+
+  const phoneAlert = (
+    <div className="container mx-auto min-h-full bg-base-200">
+      <div className="min-h-full flex flex-col justify-center space-y-4">
+        <div className="card shadow-xl text-center p-4 m-4 bg-base-100">
+        <h1 className="text-5xl text-primary"><Tv/></h1>
+        <h3 className="font-bold text-lg">Your Tv 1 is running for more than 4 hours!</h3>
+        <button className="btn btn-error">Check it out</button>
+        <button className="btn btn-ghost">Edit alert</button>
+        </div>
+      </div>
+    </div>
+  );
+
+  const section = (title:JSX.Element,description:string, phoneContent:JSX.Element, bgcolor:string, reverse:boolean) => {
+    return (
+      <div className={"hero min-h-screen "+bgcolor}>
+        <div className={"hero-content flex-col lg:flex-row justify-around pt-20 lg:pt-0"+(reverse ? " lg:flex-row-reverse":"")}>
+          {/* <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" /> */}
+          <div className={"container mx-auto max-w-md"+(reverse?"":" lg:text-right")}>
+            <div>
+              {title}
+              <p className="py-6">{description}</p>
+              <button className="btn btn-primary">Learn More</button>
+            </div>
+          </div>
+          <div className="container mx-auto max-w-sm px-4">
+            <Phone
+            content={phoneContent}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <>
     <Header/>
     {/* hero section */}
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row justify-around pt-20 lg:pt-0">
-        {/* <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" /> */}
-        <div className="container mx-auto max-w-md">
-          <div>
-            <h1 className="text-5xl font-bold">Whole-home <span className="text-primary">Energy</span><br/>Monitoring</h1>
-            <p className="py-6">Machine Learning algorithms find device signatures within your home's power profile and gradually learn about your home as things turn on and off. </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-        <div className="container mx-auto max-w-sm">
-          <Phone
-          content={phoneContent}
-          />
-        </div>
-      </div>
-    </div>
+    {section(
+      (<h1 className="text-5xl font-bold">Whole-devices <span className="text-primary">Energy</span><br/>Monitoring</h1>),
+      "Machine Learning algorithms find device signatures within your home's power profile and gradually learn about your home as things turn on and off. ",
+      phoneDevices,"bg-base-200",false)}
+    {section((<h1 className="text-5xl font-bold">Avoid<br/>Disasters<br/>with <span className="text-primary">Alerts</span></h1>),
+      "Think you forgot to turn off the Tv? Set an alert to inform you if it has been on for over 4 hours.",
+      phoneAlert,"bg-base-100",true)}
     {/* cards section */}
     <Footer/>
     </>
